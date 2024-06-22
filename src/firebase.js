8d3407fa6e2130b0ef1099b1db849c3355c5e1ba
Lakshmi-Jahnavi-Bavisetti <bljptp111@gmail.com>
2024-06-22 21:54:02 +0530

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,14 @@
 import { initializeApp } from "firebase/app";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
-import { collection, getFirestore } from "firebase/firestore";
+import { 
+    createUserWithEmailAndPassword, 
+    getAuth, 
+    signInWithEmailAndPassword, 
+    signOut
+} from "firebase/auth";
+import { addDoc, 
+    collection, 
+    getFirestore } from "firebase/firestore";
+import { toast } from "react-toastify";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD0EbS2W3SvNWrph_FE9E7TIJWy1l_dG8U",
@@ -20,13 +28,28 @@ const signup=async(name,email,password)=>{
     try {
         const res=await createUserWithEmailAndPassword(auth,email,password);
         const user=res.user;
-        await addDoc(collection(db,"user",{
+        await addDoc(collection(db,"user"),{
             uid:user.uid,
             name,
             authProvider:"local",
             email,
-        }));
+        });
     } catch (error) {
-        console.log(error)
+        console.log(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
     }
 }
+
+const login=async(email,password)=>{
+    try {
+        await signInWithEmailAndPassword(auth,email,password)
+    } catch (error) {
+        console.log(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+    }
+}
+
+const logout=async()=>{
+  signOut(auth);
+}
+export{auth,db,login,signup,logout}
